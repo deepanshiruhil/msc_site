31f9294f9c2cf0bbafffbd9a84f7e67161954ce7
deepanshiruhil <178875578+deepanshiruhil@users.noreply.github.com>
2026-06-08 16:31:16 +0530

    Replace sections in HomePage with new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
 import HeroSection from "@/components/HeroSection";
-import GallerySection from "@/components/GallerySection";
-import SponsorsSection from "@/components/SponsorsSection";
-import FAQSection from "@/components/FAQSection";
+import GoalsPage from "@/components/GoalsPage";
+import AboutUs from "@/components/AboutUs";
+import CircularGallery from "@/components/CircularGallery";
+import RecentEvents from "@/components/RecentEvents";
+import SponsorsGrid from "@/components/SponsorsGrid";
+import FAQ from "@/components/FAQ";
+import StillHaveQuestions from "@/components/StillHaveQuestions";
 
 export default function HomePage() {
   return (
     <>
       <HeroSection />
-      <GallerySection />
-      <SponsorsSection />
-      <FAQSection />
+      <GoalsPage />
+      <AboutUs />
+      <CircularGallery />
+      <RecentEvents />
+      <SponsorsGrid />
+      <FAQ />
+      <StillHaveQuestions />
     </>
   );
 }
